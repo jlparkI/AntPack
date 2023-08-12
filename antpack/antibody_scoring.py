c03823feb0b84b07fcb56101db68708c46438cbe
jlparkinson1 <jlparkinson1@gmail.com>
2023-08-12 13:18:56 -0700

    Completed the antibody scoring tool. Prebuilt HMM dbs for heavy, light and all chains and added these to manifest. Updated the setup file.

--- antpack/antibody_scoring.py
+++ antpack/antibody_scoring.py
@@ -1,14 +1,11 @@
 """Provides tools for scoring an antibody sequence against germlines of
 different species to determine which is the most likely species of origin."""
+from Bio import SeqIO
 import pyhmmer
 from pyhmmer.hmmer import hmmscan
 from pyhmmer.easel import TextSequence
 
 from .constants import allowed_inputs
-
-
-
-
 
 
 
@@ -20,20 +17,48 @@ class AntibodyScoring:
     most likely species of origin).
 
     Attributes:
-        hmm_db: A pyhmmer optimized profile.
+        all_chains_db: A pyhmmer optimized profile for all chains.
+        heavy_ig_db: A pyhmmer optimized profile for heavy chains only.
+        light_ig_db: A pyhmmer optimized profile for light chains only.
         alphabet: A pyhmmer alphabet (generally standard AAs).
     """
 
-    def __init__(self, hmm_path):
+    def __init__(self, all_chains_path = None, ig_heavy_only_path = None,
+            ig_light_only_path = None):
         """Class constructor.
 
         Args:
-            hmm_path (str): The filepath for an HMM profile (if you don't have
-                a profile, build one using IMGTBuilder).
+            all_chains_path (str): The filepath for an HMM db containing all chains,
+                including T cell receptors, for all species. If None, the default
+                db built into the package is used; the default db was constructed on 8/11/23.
+            ig_heavy_only_path (str): The filepath for an HMM db containing IG heavy chains
+                for all species. If None, the default db built into the package is used;
+                the default db was constructed on 8/11/23.
+            ig_light_only_path (str): The filepath for an HMM db containing IG light chains
+                species. If None, the default db built into the package is used;
+                the default db was constructed on 8/11/23.
         """
-        with pyhmmer.plan7.HMMFile(hmm_path) as hmm_file:
-            self.hmm_db = hmm_file.optimized_profiles()
+        with pyhmmer.plan7.HMMFile(all_chains_path) as hmm_file:
+            self.all_chains_db = hmm_file.optimized_profiles()
+        with pyhmmer.plan7.HMMFile(ig_heavy_only_path) as hmm_file:
+            self.heavy_ig_db = hmm_file.optimized_profiles()
+        with pyhmmer.plan7.HMMFile(ig_light_only_path) as hmm_file:
+            self.light_ig_db = hmm_file.optimized_profiles()
         self.alphabet = pyhmmer.easel.Alphabet.amino()
+
+
+    def _check_chain_type(self, chain_types):
+        """Checks the input chain type and returns a reference to the appropriate
+        database."""
+        if chain_types == "ALL":
+            db_file = self.all_chains_db
+        elif chain_types == "HEAVY":
+            db_file = self.heavy_ig_db
+        elif chain_types == "LIGHT":
+            db_file = self.light_ig_db
+        else:
+            raise ValueError("Unrecognized chain_types supplied.")
+        return db_file
 
 
     def _validate_sequence(self, sequence):
@@ -58,7 +83,7 @@ class AntibodyScoring:
         Raises:
             ValueError: A ValueError is raised if inappropriate inputs are detected.
         """
-        if self.hmm_db is None:
+        if self.all_chains_db is None or self.heavy_ig_db is None or self.light_ig_db is None:
             raise ValueError("An hmm profile has not been added! Consider using "
                     "IMGT Builder to generate a profile if you do not already "
                     "have one.")
@@ -71,27 +96,76 @@ class AntibodyScoring:
             self._validate_sequence(sequence)
 
 
-    def score_online_seqs(self, sequences, sequence_names, ncpu=0, skip_checks=False):
-        """Numbers and scores a list of input sequences.
+    def score_online_seqs(self, sequences, sequence_names, chain_types = "ALL",
+            ncpu=0, skip_checks=False):
+        """Scores a list of input sequences.
 
         Args:
             sequences (list): A list of strings, each of which is a sequence
                 containing the usual 20 amino acids.
             sequence_names (list): A list of strings, each of which is the
                 name of the corresponding sequence in sequences.
+            chain_types (str): One of 'ALL', 'HEAVY', 'LIGHT'. Determines which db
+                is used. ALL can be used to determine chain type as well if in doubt,
+                while HEAVY or LIGHT will be faster if you know for example that this
+                is a heavy chain.
             ncpu (int): The number of threads hmmer is allowed to use. If 0, it will
                 be autoselected.
             skip_checks (bool): If True, skip performing validity checks on the input.
                 DANGER -- setting this to true can slightly reduce overhead but may
                 cause unexpected, hard-to-diagnose errors. Think twice before setting
                 to True!
-        """
 
+        Returns:
+            score_results (list): A list of tuples (species_chain, bitscore).
+
+        Raises:
+            ValueError: A ValueError is raised if unexpected arguments are passed.
+        """
         if not skip_checks:
             self._in_memory_checks(sequences, sequence_names)
+
+        db_file = self._check_chain_type(chain_types)
 
         loaded_seqs = [TextSequence(name=name.encode(), sequence=seq).digitize(self.alphabet)
                 for (name, seq) in zip(sequence_names, sequences)]
 
         return [(h.name.decode(), h.score) for h in
-                hmmscan(self.hmm_db, loaded_seqs, cpus=ncpu)]
+                hmmscan(db_file, loaded_seqs, cpus=ncpu)]
+
+
+    def score_offline_seqs(self, fasta_file, chain_types = "ALL", ncpu=0):
+        """Scores all the sequences in a fasta file.
+
+        Args:
+            fasta_file (str): A valid filepath to a fasta file.
+            chain_types (str): One of 'ALL', 'HEAVY', 'LIGHT'. Determines which db
+                is used. ALL can be used to determine chain type as well if in doubt,
+                while HEAVY or LIGHT will be faster if you know for example that this
+                is a heavy chain.
+            ncpu (int): The number of threads hmmer is allowed to use. If 0, it will
+                be autoselected.
+
+        Returns:
+            score_results (list): A list of tuples (species_chain, bitscore).
+
+        Raises:
+            ValueError: A ValueError is raised if unexpected arguments are passed.
+        """
+        try:
+            with open(fasta_file, "r", encoding="utf-8") as fhandle:
+                pass
+        except Exception as exc:
+            raise ValueError("Invalid filepath supplied.") from exc
+
+        db_file = self._check_chain_type(chain_types)
+
+        scores = []
+        with open(fasta_file, "r", encoding="utf-8") as fhandle:
+            for record in SeqIO.parse(fhandle, 'fasta'):
+                loaded_seq = TextSequence(name=record.name.encode(),
+                        sequence=str(record.seq))
+                hits = list(hmmscan(db_file, [loaded_seq], cpus=ncpu))
+                scores.append( (hits[0].name.decode(), hits[0].score) )
+
+        return scores
--- setup.py
+++ setup.py
@@ -1,23 +1,49 @@
 """The package setup file for AntPack."""
 import os
-from setuptools import setup, find_packages
+import sys
+from subprocess import getoutput
+from distutils.ccompiler import new_compiler
+from distutils.sysconfig import customize_compiler
+from setuptools import setup, find_packages, Extension
+
+if sys.version_info[:2] < (3, 7):
+    raise RuntimeError("Python version >= 3.7 required.")
 
 
-setup_fpath = os.path.abspath(os.path.dirname(__file__))
 
 
-read_me = os.path.join(setup_fpath, "README.md")
-with open(read_me, "r", encoding="utf-8") as fhandle:
-    long_description = "".join(fhandle.readlines())
+def get_version(setup_fpath):
+    """Retrieves the version number."""
+
+    os.chdir(os.path.join(setup_fpath, "antpack"))
+    with open("__init__.py", "r", encoding="utf-8") as fhandle:
+        version_line = [l for l in fhandle.readlines() if
+                    l.startswith("__version__")]
+        version = version_line[0].split("=")[1].strip().replace('"', "")
+    os.chdir(setup_fpath)
+    return version
 
 
-setup(name='antpack',
-     version='0.0.1',
-     description='A toolkit for antibody data processing and inference',
-     packages=find_packages(),
-     data_files = ['dependencies/muscle', 'dependencies/muscle_macOS'],
-     long_description = long_description,
-     long_description_content_type="text/markdown",
-     include_package_data = True,
-     scripts=[],
+def using_clang():
+    """Check to see if we are using Clang."""
+    compiler = new_compiler()
+    customize_compiler(compiler)
+    compiler_ver = getoutput("{0} -v".format(compiler.compiler[0]))
+    return "clang" in compiler_ver
+
+
+
+if __name__ == "__main__":
+    home_dir = os.path.dirname(os.path.abspath(__file__))
+    setup(
+        name="antpack",
+        version=get_version(home_dir),
+        description="A Python package for processing, manipulating and making inferences about antibody sequence data",
+        long_description="A Python package for processing, manipulating and making inferences about antibody sequence data",
+        packages=find_packages(),
+        setup_requires=['pybind11>=2.4'],
+        install_requires=['pybind11>=2.4', "numpy", "pyhmmer"],
+        include_package_data=True,
+        license="MIT",
+        python_requires=">=3.7",
     )
