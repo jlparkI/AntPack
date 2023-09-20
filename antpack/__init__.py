3ba85831bd8f370a8ddadd998a2804c2ffec6ea2
jlparkinson1 <jlparkinson1@gmail.com>
2023-09-20 08:12:19 -0700

    Updated version number for new release.

--- antpack/__init__.py
+++ antpack/__init__.py
@@ -1,6 +1,6 @@
 #Used for determining the version if running setup.
 #Only change if building a new version.
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .consensus_update_tools import build_consensus_alignment
 from .single_chain_annotator import SingleChainAnnotator
