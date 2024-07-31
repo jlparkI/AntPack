d037fab97a5d0b6f5594cac2957bf5965ca143e9
Jonathan Parkinson <jlparkinson1@gmail.com>
2024-07-31 11:29:42 -0700

    Updated version number to prep for next release.

--- src/antpack/__init__.py
+++ src/antpack/__init__.py
@@ -1,6 +1,6 @@
 #Used for determining the version if running setup.
 #Only change if building a new version.
-__version__ = "0.2.7"
+__version__ = "0.3"
 
 from .numbering_tools import SingleChainAnnotator, PairedChainAnnotator
 from .vj_tools.vj_gene_assignment import VJGeneTool
