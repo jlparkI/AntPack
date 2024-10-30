3ab7539d7bdacdd2558636336a0bd94aefa89b6b
Jonathan Parkinson <jlparkinson1@gmail.com>
2024-10-30 10:53:24 -0700

    Removed Bio import added in error.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2024'
 author = 'Jonathan Parkinson'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.3.6.1'
 
 
 # -- General configuration ---------------------------------------------------
