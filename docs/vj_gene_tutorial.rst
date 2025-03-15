b9f21514431f4099101776c43019a8f67ebca4a6
Jonathan Parkinson <jlparkinson1@gmail.com>
2025-03-14 19:03:18 -0700

    Added test data for TCR VJ gene assignment.

--- docs/vj_gene_tutorial.rst
+++ docs/vj_gene_tutorial.rst
@@ -30,13 +30,9 @@ returns a list of the v-genes and j-genes that achieved the same
 (or essentially equivalent) score, delimited or separated by the
 character "_".
 
-AntPack can also use either the "imgt" database or the "ogrdb"
-database. In general we prefer "imgt". "ogrdb" assigns different
-names to sequences with the same amino acid sequence, so in some
-cases there is more than one name for the same gene; when this
-happens, these are all reported separated by spaces, i.e.
-"gene1 gene2". Still, the OGRDB database is available if useful
-for your particular project. Where IMGT is concerned, note that some
+In some versions of AntPack, there was a supported option to use
+the OGRDB database in place of IMGT. This option is removed as
+of v0.3.8. Note that some
 sequences found in IMGT VQuest are excluded (pseudogenes,
 partial sequences, sequences that do not have 'F' in the functionality
 section etc.), so not all V and J genes in the IMGT db are in
--- src/antpack/cpp_src/ant_ext.cpp
+++ src/antpack/cpp_src/ant_ext.cpp
@@ -271,20 +271,14 @@ NB_MODULE(antpack_cpp_ext, m) {
                 the numbered sequence matches the v-gene divided by the total number of
                 non-blank positions in the v-gene. If mode is 'evalue', the best BLOSUM
                 score (this can be converted to an e-value). If more than one v-gene
-                with the same score is found, multiple
-                v-genes are returned as a single string delimited with '_' to separate
-                the different v-genes. for the ogrdb database, where multiple names
-                have been assigned to the same aa sequence, these multiple names
-                are separated by ' ' in the output.
+                with the same score is found, multiple v-genes are returned as a single
+                string delimited with '_' to separate the different v-genes.
             j_pident (float): If mode is 'identity', the number of positions at which
                 the numbered sequence matches the j-gene divided by the total number of
                 non-blank positions in the j-gene. If mode is 'evalue', the best BLOSUM
                 score (this can be converted to an e-value). If more than one j-gene
-                with the same score is found, multiple
-                j-genes are returned as a single string delimited with '_' to separate
-                the different j-genes. for the ogrdb database, where multiple names
-                have been assigned to the same aa sequence, these multiple names
-                are separated by ' ' in the output.)") 
+                with the same score is found, multiple j-genes are returned as a single
+                string delimited with '_' to separate the different j-genes.)")
         .def("get_vj_gene_sequence",
                 &VJAssignment::VJMatchCounter::get_vj_gene_sequence,
                 nb::arg("vj_gene_name"), nb::arg("species"),
